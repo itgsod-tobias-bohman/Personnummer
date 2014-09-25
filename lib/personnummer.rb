9fde286b9a5c0946cd9e41de256267ca7fef6526
itgsod-tobias-bohman <tobias.bohman@Garbage-waste.local>
2014-09-25 10:03:54 +0200

    init
    first init

---
+++ lib/personnummer.rb
@@ -0,0 +1,3 @@
+class Personnummer
+
+end
---
+++ spec/personnummer_spec.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+require_relative '../lib/personnummer'
+
+describe Personnummer do
+
+  it 'should generate a correct personnummer' do
+
+    true.should == true
+  end
+end
